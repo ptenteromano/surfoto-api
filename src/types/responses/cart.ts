f9b750dc05b30f726092afa3ae8544e6ab9a1135
Philip Tenteromano <tenteromano.p@gmail.com>
2023-11-05 17:15:03 -0500

    add owned_or_purchased to in_cart

--- src/types/responses/cart.ts
+++ src/types/responses/cart.ts
@@ -2,12 +2,18 @@ import { PhotoPresenter } from "./photo";
 import { ErrorResponse } from "./utils";
 import { PaymentIntent } from "./payments";
 
-export type CartBasics = { photos: PhotoPresenter[]; total: number, fee: number };
+export type CartBasics = {
+  photos: PhotoPresenter[];
+  total: number;
+  fee: number;
+};
 
 export type CartIndex = CartBasics | ErrorResponse;
 export type CartWithPaymentIntent = (CartIndex & PaymentIntent) | ErrorResponse;
 export type CartCreate = { cart_item_uuid: string } | ErrorResponse;
 export type CartUpdate = { success: boolean } | ErrorResponse;
-export type CartItemInCart = { in_cart: boolean } | ErrorResponse;
+export type CartItemInCart =
+  | { in_cart: boolean; owned_or_purchased: boolean }
+  | ErrorResponse;
 
 export type Cart = CartBasics;
